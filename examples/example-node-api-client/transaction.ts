1e0302dbd0788a8f2362b1e6220044a6c52502c7
nsstarr <anastasia@tryroll.com>
2023-06-22 10:59:16 +0100

    enhancement: modify the example of sendBatchFromPlatformUser so that the user is prompted to provide transaction parameters multiple times within a while loop, allowing them to send multiple batches of transactions

--- examples/example-node-api-client/transaction.ts
+++ examples/example-node-api-client/transaction.ts
@@ -90,11 +90,113 @@ export const sendFromPlatformUser = async () => {
   }
 }
 
+// export const sendBatchFromPlatformUser = async () => {
+//   try {
+//     const sdkPool = new SDKPool(platformUserConfig)
+//     await sdkPool.getSDK(InteractionType.ClientCredentials).generateToken()
+//     const clientPool = new ClientPool({ baseUrl: process.env.API_URL }, sdkPool)
+
+//     let batchSendPrompt = true
+
+//     while (batchSendPrompt) {
+//       const answers = await inquirer.prompt([
+//         {
+//           type: 'input',
+//           name: 'userType',
+//           message: 'User Type (discord, telegram)',
+//         },
+//         {
+//           type: 'input',
+//           name: 'platformUserId',
+//           message: 'User ID from the external platform',
+//         },
+//         {
+//           type: 'input',
+//           name: 'toUsername',
+//           message: 'Roll Username to send to',
+//         },
+//         {
+//           type: 'input',
+//           name: 'tokenId',
+//           message: 'Token UUID',
+//         },
+//         {
+//           type: 'input',
+//           name: 'amount',
+//           message: 'the number of tokens to send',
+//         },
+//         {
+//           type: 'confirm',
+//           name: 'batchSendAgain',
+//           message: 'Do you want to send another batch?',
+//           default: false,
+//         },
+//       ])
+
+//       const userResp = await user.createPlatformUser(
+//         clientPool.getClient(InteractionType.ClientCredentials),
+//         {
+//           userType: answers.userType,
+//           platformUserId: answers.platformUserId,
+//         },
+//       )
+
+//       const masqueradeToken = await user.getUserMasqueradeToken(
+//         clientPool.getClient(InteractionType.ClientCredentials),
+//         {
+//           userId: userResp.userID,
+//         },
+//       )
+
+//       const clientToken = await safelyGetToken(
+//         sdkPool.getSDK(InteractionType.ClientCredentials),
+//       )
+
+//       await sdkPool
+//         .getSDK(InteractionType.MasqueradeToken)
+//         .generateToken(
+//           encodeClientMasqueradeTokens(
+//             clientToken.access_token,
+//             masqueradeToken.token,
+//           ),
+//         )
+
+//       const tx = await transaction.send(
+//         clientPool.getClient(InteractionType.MasqueradeToken),
+
+//         {
+//           amount: answers.amount,
+//           toUsername: answers.toUsername,
+//           tokenId: answers.tokenId,
+//           note: 'test transaction',
+//         },
+//       )
+
+//       printTable([
+//         {
+//           from: tx.from.username,
+//           to: tx.to.username,
+//           token: tx.token.symbol,
+//           amount: tx.amount,
+//           status: tx.status,
+//           type: tx.type,
+//         },
+//       ])
+
+//       batchSendPrompt = answers.batchSendAgain
+//     }
+//   } catch (err) {
+//     console.error(err)
+//   }
+// }
+
 export const sendBatchFromPlatformUser = async () => {
   try {
     const sdkPool = new SDKPool(platformUserConfig)
     await sdkPool.getSDK(InteractionType.ClientCredentials).generateToken()
     const clientPool = new ClientPool({ baseUrl: process.env.API_URL }, sdkPool)
+
+    const transactions = [] // Store the transactions
 
     let batchSendPrompt = true
 
@@ -133,6 +235,15 @@ export const sendBatchFromPlatformUser = async () => {
         },
       ])
 
+      const transactionParams = {
+        toUsername: answers.toUsername,
+        tokenId: answers.tokenId,
+        amount: answers.amount,
+        note: 'test transaction',
+      }
+
+      transactions.push(transactionParams) // Add the transaction to the list
+
       const userResp = await user.createPlatformUser(
         clientPool.getClient(InteractionType.ClientCredentials).call,
         {
@@ -170,16 +281,16 @@ export const sendBatchFromPlatformUser = async () => {
         },
       )
 
-      printTable([
-        {
-          from: tx.from.username,
-          to: tx.to.username,
-          token: tx.token.symbol,
-          amount: tx.amount,
-          status: tx.status,
-          type: tx.type,
-        },
-      ])
+      const transactionDetails = {
+        from: tx.from.username,
+        to: tx.to.username,
+        token: tx.token.symbol,
+        amount: tx.amount,
+        status: tx.status,
+        type: tx.type,
+      }
+
+      printTable([transactionDetails])
 
       batchSendPrompt = answers.batchSendAgain
     }
